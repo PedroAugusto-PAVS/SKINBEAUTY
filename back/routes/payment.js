65698ee91d3f6fe540cf6d9569b2d114df566e99
Pedro Augusto <121217228+PedroAugusto-PAVS@users.noreply.github.com>
2026-05-24 12:57:39 -0300

    Atualiza URL da Vercel

--- back/routes/payment.js
+++ back/routes/payment.js
@@ -54,7 +54,7 @@ router.post('/create', async (req, res) => {
           process.env.INFINITE_HANDLE,
 
           redirect_url:
-          'https://public-dvz5gszu9-pedrinhogv55-6900s-projects.vercel.app/success.html',
+          'https://public-delta-sage-68.vercel.app/success.html',
 
         webhook_url:
           ' https://skinbeauty.onrender.com/webhook',
