7e94beb8e6abb3812cdb21bef8462fac99f841d9
Pedro Augusto <121217228+PedroAugusto-PAVS@users.noreply.github.com>
2026-05-24 12:38:02 -0300

    Corrige caminho das rotas

--- back/server.js
+++ back/server.js
@@ -3,24 +3,17 @@ const cors = require('cors');
 
 require('dotenv').config();
 
-const paymentRoute =
-require('../routes/payment');
-
-const webhookRoute =
-require('../routes/webhook');
+const paymentRoute = require('./routes/payment');
+const webhookRoute = require('./routes/webhook');
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
 app.use('/payment', paymentRoute);
-
 app.use('/webhook', webhookRoute);
 
-app.listen(process.env.PORT, () => {
-  console.log(
-    `Servidor rodando na porta ${process.env.PORT}`
-  );
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Servidor rodando');
 });
