e996a1567a0064d230e37b40a408d062567d9b5c
Pedro Augusto <121217228+PedroAugusto-PAVS@users.noreply.github.com>
2026-05-29 08:42:45 -0300

    Atualizar o payment.js

--- back/routes/payment.js
+++ back/routes/payment.js
@@ -19,7 +19,7 @@ router.post('/create', async (req, res) => {
     const plans = {
       glow: {
         name: 'Plano Glow',
-        price: 1000 // centavos
+        price: 100 // centavos
       },
 
       duo: {
