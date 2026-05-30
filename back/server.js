6dbedc3872f0c2db998fd93276a0bc4731e92062
Pedro Augusto <121217228+PedroAugusto-PAVS@users.noreply.github.com>
2026-05-30 16:01:20 -0300

    Corrige ordem do app no server

--- back/server.js
+++ back/server.js
@@ -1,7 +1,3 @@
-//tela de admin
-const adminRoute = require('./routes/admin');
-app.use('/admin', adminRoute);
-
 const express = require('express');
 const cors = require('cors');
 
@@ -9,19 +5,21 @@ require('dotenv').config();
 
 const paymentRoute = require('./routes/payment');
 const webhookRoute = require('./routes/webhook');
+const adminRoute = require('./routes/admin');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/payment', paymentRoute);
-app.use('/webhook', webhookRoute);
-
 app.get('/', (req, res) => {
   res.send('API SkinBeauty rodando ✅');
 });
 
+app.use('/payment', paymentRoute);
+app.use('/webhook', webhookRoute);
+app.use('/admin', adminRoute);
+
 app.listen(process.env.PORT || 3000, () => {
   console.log('Servidor rodando');
-}); 
+});
